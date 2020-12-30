0c4cda645fd708be51a5ae21ec1c92937d010849
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-30 20:11:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,4 +4,17 @@ def game_hash
   game_hash = {
     :home => {
       :team_name =>"", 
-      :colors = [], :players =>[{:player_name =>""},{:number =>0},{:shoe =>0},{:points =>0},{:rebouds =>0},{:assists =>0},{:steals =>0},{:blocks =>0},{:slam_dunks =>0}]}, :away => {}}
+      :colors = [], 
+      :players =>[
+        {:player_name =>""},
+        {:number =>0},
+        {:shoe =>0},
+        {:points =>0},
+        {:rebouds =>0},
+        {:assists =>0},
+        {:steals =>0},
+        {:blocks =>0},
+        {:slam_dunks =>0}
+        ]
+    },
+        :away => {}}
